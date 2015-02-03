8397dcde867fecb91fed8fc4473beb2d3da76ad3
usuario <usuario@Drath>
2015-02-02 21:03:38 -0600

    Listo @daton

---
+++ src/Tareas/Tarea3/CalcularArea.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tareas.Tarea3;
+
+/**
+ *
+ * @author usuario
+ */
+public class CalcularArea {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Cuadrado cuadrado=new Cuadrado();
+    cuadrado.setFigura("rectangulo");
+    cuadrado.setAncho(7);
+    cuadrado.setLargo(7);
+    cuadrado.Figura();
+    
+         
+      
+    }
+    
+}
