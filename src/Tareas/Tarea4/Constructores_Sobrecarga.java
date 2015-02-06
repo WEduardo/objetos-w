e73fe2799b2755de27cdd896498e846e1b4ebb59
usuario <usuario@Drath>
2015-02-06 05:10:15 -0600

    Listo @daton

---
+++ src/Tareas/Tarea4/Constructores_Sobrecarga.java
@@ -0,0 +1,14 @@
+
+
+
+package Tareas.Tarea4;
+
+
+public class Constructores_Sobrecarga {
+
+    public static void main(String[] args) {
+       
+ /* ESTAS CLASES Y MAINS SERA PARA EXPLICAR LAS REGLAS DE LOS CONSTRUCTORES */
+    }
+    
+}
---
+++ src/Tareas/Tarea4/Reglas_regla1.java
@@ -0,0 +1,19 @@
+
+package Tareas.Tarea4;
+
+
+public class Reglas_regla1 {
+
+    
+    
+    public static void main(String[] args) {
+        
+        Regla1 Objetonuevo= new Regla1();
+       
+        /* CON AYUDA DEL CONSTRUCTOR SE CREAN LOS OBJETOS EN ESTE CASO EL
+        CONSTRUCTOR POR DEFECTO ES Regla1();
+        */
+        
+    }
+    
+}
