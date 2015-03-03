3e5ffb595c5d2f57cd42a9e252fb137efeac3a92
T-107 <T-107@PC246>
2015-03-02 19:31:14 -0600

    @WEduardo

---
+++ src/ETE/GenerarAnimales.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ETE;
+
+
+
+
+import java.util.*;
+
+public class GenerarAnimales {
+    
+    
+    public static ArrayList<Animal> generarAnimal(){
+        ArrayList<Animal> animales= new ArrayList<Animal>();
+        
+        Animal a1=new Animal("leon",2);
+        Animal a2=new Animal("delfin",6);
+                       
+                       
+                      
+         animales.add(a1);
+          animales.add(a2);
+          
+                        
+                 return animales;
+                        
+                        
+                        
+                        
+                        
+    }
+}
